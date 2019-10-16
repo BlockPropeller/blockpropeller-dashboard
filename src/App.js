1cd67eb708ae0493a9c5e57a290fa183915df59b
Miljan Tekic <miljan.tekic@gmail.com>
2019-10-16 16:21:22 +0200

    Updated readme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button} from "semantic-ui-react";
+import {Button, Container, Menu} from "semantic-ui-react";
 
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
@@ -7,7 +7,19 @@ import './App.css';
 function App() {
     return (
         <div className="App">
+            <Menu>
+                <Container>
+                  <Menu.Item as='a'>
+                    <span>Testiram</span>
+                  </Menu.Item>
+                  <Menu.Item as='a' active>
+                    <span>Active</span>
+                  </Menu.Item>
+                </Container>
+            </Menu>
+          <Container>
             <Button primary>Click Here</Button>
+          </Container>
         </div>
     );
 }
