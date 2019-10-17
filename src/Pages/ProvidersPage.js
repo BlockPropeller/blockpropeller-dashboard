26d96988036ca5da174e55ba5b6c3e31a8e1239b
Miljan Tekic <miljan.tekic@gmail.com>
2019-10-17 15:56:07 +0200

    Base for providers page

---
+++ src/Pages/ProvidersPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ProvidersPage extends Component {
+    render() {
+        return (
+            <div>
+                Pro
+            </div>
+        );
+    }
+}
+
+export default ProvidersPage;
